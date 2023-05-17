f9c2adbcd933cf6f9500a9d8f4be0aa11171d0c8
Arsalan Bashir <arsalanbashir831@gmail.com>
2023-05-17 23:25:54 +0500

    SelectImplemented

--- src/widgets/SelectPlan.jsx
+++ src/widgets/SelectPlan.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { getJoin } from '../global';
 import { useTheme } from '@mui/material/styles';
-import Calendar from 'react-calendar'; 
+import Calendar from 'react-calendar';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import 'react-calendar/dist/Calendar.css';
 
 const ITEM_HEIGHT = 48;
@@ -33,7 +37,7 @@ function getStyles(name, companyName, theme) {
 
 const SelectPlan = () => {
   const theme = useTheme();
- 
+
   const [companyDetails, setCompanyDetails] = useState({
     name: '',
     url: '',
@@ -42,6 +46,7 @@ const SelectPlan = () => {
     meetingDate: '',
   });
   const [availableDates, setAvailableDates] = useState([]);
+  const [companyName, setCompanyName] = useState('');
 
   const handleInputChange = (event) => {
     const { id, value } = event.target;
@@ -65,80 +70,115 @@ const SelectPlan = () => {
       meetingDate: date,
     }));
   };
+
   return (
     <div className="bg-white shadow-md rounded px-8 py-6 max-w-md mx-auto">
-
-{getJoin() === 'company' &&(
-<>
-<h1>No Plans For Company Click For Next</h1>
-</>
-)}
+      {getJoin() === 'company' && (
+        <>
+          <h1>No Plans For Company Click For Next</h1>
+        </>
+      )}
       {getJoin() === 'team' && (
         <>
           {/* Render the input fields and calendar for team */}
-          <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyName">
-            Company Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={companyDetails.name}
-            onChange={handleInputChange}
-            placeholder="Enter company Name"
-          />
+          <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+            <Select
+              displayEmpty
+              value={companyName}
+              onChange={(event) => setCompanyName(event.target.value)}
+              input={<OutlinedInput />}
+              renderValue={(selected) => {
+                if (!selected) {
+                  return <em>Select Company</em>;
+                }
+                return selected;
+              }}
+              MenuProps={MenuProps}
+              inputProps={{ 'aria-label': 'Without label' }}
+            >
+              <MenuItem disabled value="">
+                <em>Select Company</em>
+              </MenuItem>
+              {names.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, companyName, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
-          <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyUrl">
-            Company URL
-          </label>
-<input
-         type="text"
-         id="url"
-         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         value={companyDetails.url}
-         onChange={handleInputChange}
-         placeholder="Enter company URL"
-       />
-             <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyAddress">
-        Company Address
-      </label>
-      <input
-        type="text"
-        id="address"
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        value={companyDetails.address}
-        onChange={handleInputChange}
-        placeholder="Enter company Address"
-      />
+          {companyName === 'Not Mention' && (
+            <>
+              <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyName">
+                Company Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={companyDetails.name}
+                onChange={handleInputChange}
+                placeholder="Enter company Name"
+              />
 
-      <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyDescription">
-        Company Description
-      </label>
-      <textarea
-        id="description"
-        className="border border-gray-300 rounded p-2 resize-none focus:outline-none focus:ring focus:border-blue-500"
-        rows={6}
-        cols={45}
-        value={companyDetails.description}
-        onChange={handleInputChange}
-        placeholder="Enter your text"
-      />
+              <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyUrl">
+                Company URL
+              </label>
+              <input
+                type="text"
+                id="url"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={companyDetails.url}
+                onChange={handleInputChange}
+                placeholder="Enter company URL"
+              />
 
-      <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="teamMeeting">
-        Schedule Meeting
-      </label>
-      <Calendar
-        value={companyDetails.meetingDate}
-        onClickDay={handleSelectDate}
-        tileClassName={({ date, view }) =>
-          view === 'month' && availableDates.includes(date) ? 'available' : null
-        }
-        onChange={handleCalendarChange}
-      />
-    </>
-  )}
-</div>
-);
+              <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyAddress">
+                Company Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={companyDetails.address}
+                onChange={handleInputChange}
+                placeholder="Enter company Address"
+              />
+
+              <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="companyDescription">
+                Company Description
+              </label>
+              <textarea
+                id="description"
+                className="border border-gray-300 rounded p-2 resize-none focus:outline-none focus:ring focus:border-blue-500"
+                rows={6}
+                cols={45}
+                value={companyDetails.description}
+                onChange={handleInputChange}
+                placeholder="Enter your text"
+              />
+
+              <label className="block text-gray-700 font-bold mb-2 my-2" htmlFor="teamMeeting">
+                Schedule Meeting
+              </label>
+              <Calendar
+                value={companyDetails.meetingDate}
+                onClickDay={handleSelectDate}
+                tileClassName={({ date, view }) =>
+                  view === 'month' && availableDates.includes(date) ? 'available' : null
+                }
+                onChange={handleCalendarChange}
+              />
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default SelectPlan;
